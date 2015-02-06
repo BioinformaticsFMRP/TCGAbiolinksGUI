58d7c3b43df87980e099d78e2535958ac3339315
tiago <tiagochst@gmail.com>
2015-02-06 21:44:11 -0200

    Correcting coverage script. Removing useless old files that were wrongly committed

--- coverage/coverage.R
+++ coverage/coverage.R
@@ -1,10 +1,10 @@
-loc <- file.path("/Users/tiago/prime")
-
+# Load a unit testing framework and testCoverage.
 require(testthat)
+require("testCoverage")
+loc <- file.path(".")
 
 # example saturate - testthat partial coverage
 reportCoverage(sourcefiles = list.files(file.path(loc, "R"), full.names = TRUE),
-               executionfiles = list.files(file.path(loc, 
-                                                      "tests", "testthat"), full.names = TRUE), 
-               reportfile = "testCoverage_saturate_prime.html", 
+               executionfiles = list.files(file.path(loc,"tests", "testthat"), full.names = TRUE), 
+               reportfile = "coverage/results/test_coverage.html", 
                writereport = TRUE, clean = TRUE)
