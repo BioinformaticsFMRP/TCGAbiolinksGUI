52c62c215460dd5d71ba73d012c0d4c021c6db09
tiago <tiagochst@gmail.com>
2015-02-09 21:56:00 -0200

    Encode.R documentation started TODO: 1 - target can receive more than one filter, this need to be handled 2 - treat NULL values in the input

--- R/encode.R
+++ R/encode.R
@@ -1,57 +1,60 @@
 #' @title Download data from ENCODDE project
 #' @description
-#' @param x A number
-#' @return TRUE if \code{x} is prime, FALSE otherwise
+#' @param searchTerm - bone chip
+#' @param type - "experiment" "publication" "software" ""antibody_lot" "web"
+#' @param target - "transcription factor" "RNA binding protein" "tag" "histone modification"
+#' @param output - path to save files
 #' @examples
-#' prime(4)
-#' prime(13)
-#' \dontrun{
-#'    prime('a')
-#' }
+#' encodeDownload("bone chip",
+#'                "experiment",
+#'                "transcription factor", 
+#'                "encodeDownload"
+#'                )
 #' @seealso \url{https://www.encodeproject.org/search/}
 #' @seealso \url{https://www.encodeproject.org/help/rest-api/}
 #' 
-
-
 library(RCurl)
-url <- "ftp://hgdownload.cse.ucsc.edu/goldenPath/"
-filenames <- getURL(url,ftp.use.epsv = FALSE,dirlistonly = TRUE) 
-filenames = paste(url, strsplit(filenames, "\n")[[1]], sep = "")
-
-
-library(RJSONIO)
 library(rjson)
 
-# FIXED
-encodeProject = "https://www.encodeproject.org"
-encodeSearch  = "/search/"
-returnParam   = "format=json"
-frameParam    = "frame=embedded"
+# USER SELECTION - needs improvement
+searchTerm  = "bone chip"
+type        = "experiment"
+target      = "transcription factor" # can be more than one
+output      = "encodeDownload"
 
-# USER SELECTION
-searchParams = "searchTerm=bone+chip"
-type         = "type=experiment"
-target       = "target.investigated_as=transcription%20factor"
+encodeDownload <- function(search, type, target, output) {
 
-URLoptions <- paste (searchParams, returnParam, frameParam, target, type, sep="&")
-URL        <- paste (encodeProject, paste( encodeSearch, URLoptions, sep="?"), sep="")
-
-filenames  <- getURL (URL, dirlistonly = TRUE) 
-data       <- fromJSON (filenames, unexpected.escape = "keep")
-
-# Downloading folder
-dir.create("encodeDownload")
-
+  # Constant parameters
+  encodePath  = "https://www.encodeproject.org"
+  searchPath  = "/search/"
+  format      = "format=json"
+  frame       = "frame=embedded"
   
-# Downloads all files from the search
-for (j in 1:length (data$'@graph')){ 
-  for (i in 1:length (data$'@graph'[[j]]$files)){
-    aux <- data$'@graph'[[j]]$files[[i]]$href
-    dn <- paste (encodeProject, aux, sep = "")
-    curl = getCurlHandle()
-    getURL (dn, curl = curl)
-    dn <- getCurlInfo(curl)$redirect.url
-    rm(curl) # release the curl!
-    download.file( dn, paste("encodeDownload",unlist(strsplit(aux,"/"))[5],sep="/"), method="curl")
+  searchTerm  = paste ("searchTerm",  gsub(" ","+",search), sep="=")
+  type        = paste ("type", type, sep="=")
+  target      = paste ("target.investigated_as", target,sep="=")
+  
+  URLoptions <- paste (searchTerm, format, frame, target, type, sep="&")
+  URL        <- paste (encodePath, paste( searchPath, URLoptions, sep="?"), sep="")
+  
+  data       <- fromJSON (
+                  getURL (URL, dirlistonly = TRUE), 
+                  unexpected.escape = "keep"
+                )
+
+  # Output folder
+  dir.create(output)
+  
+  # Downloads all files from the search
+  for (j in 1:length (data$'@graph')){ 
+    for (i in 1:length (data$'@graph'[[j]]$files)){
+      curl = getCurlHandle()
+      dn <- paste (encodeProject, data$'@graph'[[j]]$files[[i]]$href, sep = "")
+      getURL (dn, curl = curl)
+      dn <- getCurlInfo(curl)$redirect.url
+      rm(curl) # release the curl!
+      download.file( dn, paste("encodeDownload",unlist(strsplit(aux,"/"))[5],sep="/"), method="curl")
+      rm(dn)
+    }
   }
 }
