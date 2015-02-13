141be292f9b936a55200e68e29ac208ef92dbe78
tiago <tiagochst@gmail.com>
2015-02-13 20:13:23 -0200

    Starting to treat project as biomics Roadmap working, need UI Function eGeo working as: a <- eGeo(c("h1 cell","All Fields",NULL),c("roadmap epigenomics","Project","AND"),c("gsm","Filter","AND"))
    Use this site to build query http://www.ncbi.nlm.nih.gov/gds/advanced
    gsm filter gets only samples

--- R/roadmap.R
+++ R/roadmap.R
@@ -1,89 +1,53 @@
-#' term=RRBS%5BAll%20Fields%5D%20AND%20H1%5BAll%20Fields%5D%20AND%20%22gsm%22%5BFilter%5D%20&cmd=DetailsSearch
-#' RRBS[All Fields] AND H1[All Fields] AND roadmap epigenomics[Project]
-# http://www.ncbi.nlm.nih.gov/geo/info/geo_paccess.html
-# X[[3]] = Connector (AND,OR, NOT)
-# X[[2]] = Field [Project]
-# X[[1]] = Term "Rattus norvegicus"
-searchGeo <- function(...) {
-  input_list <- list(...)
-  output_list <- lapply(X=input_list, function(x) {
-    if(length(x) > 1){
-      paste(paste(x[[1]],gsub(" ","%20",x[[2]]),sep="["),NULL,sep="]")
-    }
-    else{
-      paste(paste(NULL,x[[1]],sep="("),NULL,sep=")")
-    }
-  })
-  output_list <- paste(output_list,collapse="")
-  return(output_list)
-}
-
+#' Create query from UI to be used with esearch - not completed
+#' Create query from UI to be used with esearch - not completed
+#'
+#' @param List of parameters parameter = (Term,Fild,Connector)
+#' @keywords internal
 eGeo <- function(...) {
   input_list <- list(...)
   output_list <- lapply(X=input_list, function(x) {
-    if(length(x) > 1){
+    if(length(x) > 2){
       paste(x[[3]],paste(paste(x[[1]],x[[2]],sep="["),NULL,sep="]"))
     }
     else{
       paste(paste(NULL,x[[1]],sep="("),NULL,sep=")")
     }
   })
-  output_list <- paste(output_list,collapse="")
+  output_list <- paste(output_list,collapse=" ")
   return(output_list)
 }
 
-#' @title Download data from ENCODDE project
-#' @description Downloads data from ENCODE project
-#' @param iSearch - bone chip
-#' @param iType -   "experiment" "publication" "software" ""antibody_lot" "web"
-#' @param iTarget - "transcription factor" "RNA binding protein" 
-#'                  "tag" "histone modification"
-#' @param iSample - "tissue" "primary cell"
-#' @param iFType  - "bam" "bigWig" "bed_broadPeak" "broadPeak" "fastq"
-#' @param iAssay - "ChIP-seq" "RIP-chip" "Repli-chip" 
-#' @param iAssembly - "hg19" "mm9"
+#' @title Download data from GEO database
+#' @description Download data from GEO database using reutils library
+#' @param iQuery - GEO query - build it with http://www.ncbi.nlm.nih.gov/gds/advanced
 #' @param iOut - path to save files
 #' @examples
 #' \dontrun{
-#'    encodeDownloader("bone chip",
-#'                    "experiment",
-#'                    "transcription factor", 
-#'                    "primary cell",
-#'                    "bigWig",
-#'                    "ChIP-seq",
-#'                    "mm9",
-#'                    "encodeDownload"
+#'    geoDownloader ("'(h1[All Fields] AND RRBS[All Fields]) 
+#'                    AND roadmap epigenomics[Project] AND "gsm"[Filter]',
+#                     "path_to_download_folder"
 #'                    )
 #' }
-#' @seealso \url{https://www.encodeproject.org/search/}
-#' @seealso \url{https://www.encodeproject.org/help/rest-api/}
-#' @name encodeDownloader
-#' 
-
-
-# GSE[ETYP]+AND+"published+last+3+months"[Filter]
-
-geoDownloader <- function(iSearch, iType, iTarget,
-                             iSample, iFType, iAssay,
-                             iAssembly, iOut)
+#' @seealso \url{http://www.ncbi.nlm.nih.gov/geo/info/download.html}
+#' @seealso \url{http://www.ncbi.nlm.nih.gov/geo/info/geo_paccess.html}
+#' @seealso \url{http://www.ncbi.nlm.nih.gov/books/NBK25499/#chapter4.ESearch}
+#' @name geoDownloader
+geoDownloader <- function(iQuery, iOut)
   {
   
   # Constant parameters
   roadmapURL  <- "http://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=gds&term="
   optionRet    <- "retmax=5000"
   optionHist   <- "usehistory=y"
-  
-  search <- searchGeo(c("h1 RRBS",NULL,NULL),c("roadmap epigenomics","Project","AND"),c("gsm","Filter","AND"))
 
-  query <- '(h1[All Fields] AND RRBS[All Fields]) AND roadmap epigenomics[Project] AND "gsm"[Filter]'
-  pmids <- esearch(query, "gds")
+  pmids <- esearch(iQuery, "gds", retmax =  100000)
   content <- content(esummary(pmids), "parsed")
   ftps <- lapply(content, function(x)  x$FTPLink) 
-  dir.create ("downloadRM", showWarnings = FALSE)
+  dir.create (iOUt, showWarnings = FALSE)
 
   for(ftp in ftps){
     dirName <- unlist( strsplit(ftp,"/"))
-    dirName <- paste("downloadRM",dirName[length(dirName)],sep="/")
+    dirName <- paste(iOut,dirName[length(dirName)],sep="/")
     dir.create (dirName, showWarnings = FALSE)
     file <- unlist( strsplit(getURL(paste(ftp,"suppl/",sep=""))," "))
     file <- file[length(file)]                          
--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,3 @@
 library(testthat)
 
-test_check("encodeDownloader")
+test_check("biOMICs")
