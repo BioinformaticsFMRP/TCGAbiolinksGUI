ac7b7c86c4ec80b5f0410cf10bc65c1cb756fd52
tiago <tiagochst@gmail.com>
2015-02-15 18:10:24 -0200

    Hiding internal function with prefix .

--- R/encode.R
+++ R/encode.R
@@ -1,9 +1,4 @@
-#' Concatenate Target term for search
-#' Concatenate Target term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatTarget <- function(iTarget){
+.formatTarget <- function(iTarget){
   if( !is.null(iTarget)) {
     return (paste(
                   "target.investigated_as",
@@ -16,12 +11,7 @@ formatTarget <- function(iTarget){
   return(NULL)
 }
 
-#' Concatenate Sample term for search
-#' Concatenate Sample term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatSample <- function(iSample){
+.formatSample <- function(iSample){
   if (!is.null(iSample)) {
     return (paste(
                   "replicates.library.biosample.biosample_type",
@@ -33,12 +23,7 @@ formatSample <- function(iSample){
   return (NULL)
 }
 
-#' Concatenate Assay term for search
-#' Concatenate Assay term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatFType <- function(iFType){
+.formatFType <- function(iFType){
   if (!is.null(iFType)) {
     return (paste(
                   "files.file_format",
@@ -48,14 +33,9 @@ formatFType <- function(iFType){
             )
   }
   return (NULL)
-} 
+}
 
-#' Concatenate Assay term for search
-#' Concatenate Assay term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatAssay <- function(iAssay){
+.formatAssay <- function(iAssay){
   if( !is.null(iAssay)) {
     return (paste(
                   "assay_term_name",
@@ -67,12 +47,7 @@ formatAssay <- function(iAssay){
   return (NULL)
 }
 
-#' Concatenate Assembly term for search
-#' Concatenate Assembly term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatAssembly <- function(iAssembly){
+.formatAssembly <- function(iAssembly){
   if(!is.null(iAssembly)){
     return (paste(
                   "assembly",
@@ -84,25 +59,13 @@ formatAssembly <- function(iAssembly){
   return(NULL)
 }
 
-
-#' Concatenate search term for search
-#' Concatenate search term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatSearch <- function(iSearch){
+.formatSearch <- function(iSearch){
   if(!is.null(iSearch))
     return (paste ("searchTerm",  gsub(" ","+",iSearch), sep="="))
   return (NULL)
 }
 
-
-#' Concatenate type term for search
-#' Concatenate type term for search
-#'
-#' @param list to concatenate
-#' @keywords internal
-formatType <- function(iType){
+.formatType <- function(iType){
   if(!is.null(iType))
     return (paste ("type", iType, sep="="))
   return (NULL)
@@ -112,18 +75,18 @@ formatType <- function(iType){
 #' @description Downloads data from ENCODE project
 #' @param iSearch - bone chip
 #' @param iType -   "experiment" "publication" "software" ""antibody_lot" "web"
-#' @param iTarget - "transcription factor" "RNA binding protein" 
+#' @param iTarget - "transcription factor" "RNA binding protein"
 #'                  "tag" "histone modification"
 #' @param iSample - "tissue" "primary cell"
 #' @param iFType  - "bam" "bigWig" "bed_broadPeak" "broadPeak" "fastq"
-#' @param iAssay - "ChIP-seq" "RIP-chip" "Repli-chip" 
+#' @param iAssay - "ChIP-seq" "RIP-chip" "Repli-chip"
 #' @param iAssembly - "hg19" "mm9"
 #' @param iOut - path to save files
 #' @examples
 #' \dontrun{
 #'    encodeDownloader("bone chip",
 #'                    "experiment",
-#'                    "transcription factor", 
+#'                    "transcription factor",
 #'                    "primary cell",
 #'                    "bigWig",
 #'                    "ChIP-seq",
@@ -138,75 +101,68 @@ encodeDownloader <- function(iSearch, iType, iTarget,
                              iSample, iFType, iAssay,
                              iAssembly, iOut)
   {
-  
+
   # Constant parameters
   encodePath  <- "https://www.encodeproject.org"
   searchPath  <- "/search/"
   format      <- "format=json"
   frame       <- "frame=embedded"
-  
-  searchTerm <- formatSearch   ( iSearch  )
-  type       <- formatType     ( iType    )
-  target     <- formatTarget   ( iTarget  )
-  biosample  <- formatSample   ( iSample  )
-  fType      <- formatFType    ( iFType   )
-  assay      <- formatAssay    ( iAssay   )
-  assembly   <- formatAssembly ( iAssembly)
-  
+
+  searchTerm <- .formatSearch   ( iSearch  )
+  type       <- .formatType     ( iType    )
+  target     <- .formatTarget   ( iTarget  )
+  biosample  <- .formatSample   ( iSample  )
+  fType      <- .formatFType    ( iFType   )
+  assay      <- .formatAssay    ( iAssay   )
+  assembly   <- .formatAssembly ( iAssembly)
+
   URLoptions <- paste (searchTerm, format, frame, target,
-                       type, biosample, fType, assay, assembly, 
+                       type, biosample, fType, assay, assembly,
                        sep="&")
-  URL        <- paste (encodePath, 
+  URL        <- paste (encodePath,
                        paste( searchPath, URLoptions, sep="?"),
                        sep="")
-  
+
   data       <- rjson::fromJSON (
-    RCurl::getURL (URL, 
+    RCurl::getURL (URL,
                    dirlistonly = TRUE,
                    .opts = list(ssl.verifypeer = FALSE)
                    ),
     unexpected.escape = "keep"
   )
-  
+
   if (data$total > 0){
     # Output folder
     dir.create (iOut, showWarnings = FALSE)
-  
+
     # Downloads all files from the search
     for (j in 1:length (data$'@graph')){
       nbFiles <- length (data$'@graph'[[j]]$files)
       print(paste0("Downloading experiment ", j))
-    
+
       for (i in 1:nbFiles){
         filePath <- data$'@graph'[[j]]$files[[i]]$href
         dn <- paste0 (encodePath, filePath)
-        fileOut <- paste( iOut, 
+        fileOut <- paste( iOut,
                         unlist( strsplit( data$'@graph'[[j]]$'@id', "/"))[3],
                         sep = "/")
         dir.create (fileOut, showWarnings = FALSE)
-        fileOut <- paste( fileOut, 
-                          unlist( strsplit( filePath, "/"))[5], 
+        fileOut <- paste( fileOut,
+                          unlist( strsplit( filePath, "/"))[5],
                           sep = "/")
-      
-        # Did the user select a file format  
-        if(is.null(iFType) || 
+
+        # Did the user select a file format
+        if(is.null(iFType) ||
              data$'@graph'[[j]]$files[[i]]$file_format %in% iFType
           ){
-          download( dn, fileOut, quiet = TRUE)
+          downloader::download( dn, fileOut, quiet = TRUE)
           print(paste0 ("Downloaded file: ", fileOut))
         }
-      
+
       }
     }
-    print("Downloaded")  
-  } else { 
+    print("Downloaded")
+  } else {
       print(data$notification)
   }
-}
-
-#' Calls UI interface
-#' Calls UI interface
-#' @keywords internal
-encodeApp <- function() {
-  shinyApp(  server = encodeServer, ui = encodeUI)
 }
