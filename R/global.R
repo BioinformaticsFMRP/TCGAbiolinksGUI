d42888f600aff8ab3e622a5c597c7bebbcdd3913
tiago <tiagochst@gmail.com>
2015-02-27 15:37:11 -0300

    Improving onAttach. Should file be updated on the user side?

--- R/global.R
+++ R/global.R
@@ -3,33 +3,34 @@
 # @keywords internal
 .result <- reactiveValues(g_nbFiles = 0, g_downloaded = 0, df = NULL, downloading = 1)
 
-.onLoad <- function (libname, pkgname){
-  load(file = system.file("extdata/roadmap.RData",package="biOMICs"))
+.onAttach <- function (libname, pkgname){
+  .updateRoadmap()
+  load(file = system.file("extdata/roadmap.RData", package="biOMICs"), .GlobalEnv)
 }
 
 .updateRoadmap <- function (){
   #get raodmap data
-  roadmapSummaryFile <- "roadmap.csv"
-  # If file exists and it was downlaoded 1 day ago ignore
+  roadmapSummaryFile <- system.file("extdata/roadmap.RData", package="biOMICs")
+  dataDirecotry <- system.file("extdata", package="biOMICs")
+
+  # If file exists and it was downlaoded 7 day ago ignore
   # otherwise redownload it
   if (file.exists(roadmapSummaryFile)){
     finf <- file.info(roadmapSummaryFile, extra_cols = FALSE)
-    if (nrow(finf[difftime(Sys.time(), finf[,"mtime"], units = "days") > 1 , 1:4]) == 2){
+    if (nrow(finf[difftime(Sys.time(), finf[,"mtime"], units = "days") > 7 , 1:4]) == 2){
       print("file needs update")
       downloader::download("http://www.ncbi.nlm.nih.gov/geo/roadmap/epigenomics/?view=samples&sort=acc&mode=csv",
-               roadmapSummaryFile)
+                           paste0(dataDirecotry,"/roadmap.csv"))
+      roadmap <- NULL
+      roadmap$summary        <- read.csv(roadmapSummaryFile)
+      roadmap$experimentList <- levels(unique(roadmap$summary$Experiment))
+      roadmap$samplesList    <- levels(unique(roadmap$summary$Sample.Name))
+      save(roadmap,file = paste0(dataDirecotry,"/roadmap.RData"))
+      file.remove (paste0(dataDirecotry,"/roadmap.csv"))
     }
-  } else{
-    print("no file")
-    downloader::download("http://www.ncbi.nlm.nih.gov/geo/roadmap/epigenomics/?view=samples&sort=acc&mode=csv",
-             roadmapSummaryFile)
   }
 
-  roadmap <- NULL
-  roadmap$summary        <- read.csv(roadmapSummaryFile)
-  roadmap$experimentList <- levels(unique(roadmap$summary$Experiment))
-  roadmap$samplesList    <- levels(unique(roadmap$summary$Sample.Name))
-  save(roadmap,file = "inst/extdata/roadmap.RData")
+
 }
 
 #' @title biOMICs interface
