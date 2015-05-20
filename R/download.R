7eeb88d7a0c7580cf7854d76b803734d48e2ac39
Tiago Chedraoui Silva <tiagochst@gmail.com>
2015-05-20 17:11:19 -0300

    update

--- R/download.R
+++ R/download.R
@@ -153,18 +153,24 @@ roadmapDownload <- function(lines, type = NULL, path = ".") {
 #' @description Download data previously selected using the TCGASeach
 #' @param data TCGASearch output
 #' @param path location of the final data saving
+#' @param type Get files with type pattern instead of downloading all the folder
 #' @seealso TCGASearch
 #' @examples
-#'    TCGADownload(data,'folder')
+#' \dontrun{
+#'    tcgaDownload(data,path='folder',type="rsem.isoforms.results")
+#' }
 #' @export
 #' @importFrom downloader download
 #' @return Download tcga into path
-tcgaDownload <- function(data = NULL, path = ".") {
+tcgaDownload <- function(data = NULL, path = ".", type = NULL) {
+
     dir.create(path, showWarnings = FALSE)
     root <- "https://tcga-data.nci.nih.gov"
-    if (!("file" %in% colnames(data))) {
-        message("Downloading folders")
+
+
+    if (is.null(type)) {
         for (i in 1:nrow(data)) {
+
             file <- paste0(path, "/", basename(data[i, "deployLocation"]))
             message(paste0("Downloading:",
                            basename(data[i, "deployLocation"])))
@@ -175,15 +181,26 @@ tcgaDownload <- function(data = NULL, path = ".") {
             }
         }
     } else {
-        message("Downloading files")
+
         for (i in 1:nrow(data)) {
-            file <- paste0(path, "/", basename(data[i, "file"]))
-            message(paste0("Downloading:", basename(data[i, "file"])))
-            if (!file.exists(file)) {
-                download(paste0(root, gsub(".tar.gz",
-                                        "",
-                                        data[i, "deployLocation"]),
-                                "/", data[i,"file"]), file)
+
+            folder <- gsub(".tar.gz","",basename(data[i,]$deployLocation))
+            dir.create(file.path(path,folder), showWarnings = FALSE)
+            url <- gsub(".tar.gz","",data[i,]$deployLocation)
+            files <- getFileNames(paste0(root,url))
+            files <- files[grepl(type,files)]
+            if (length(files) == 0) {
+                next
+            }
+            message(paste0("Downloading:", length(files), " files"))
+
+            for (i in seq_along(files)) {
+                if (!file.exists(files[i])) {
+                    download(paste0(root,url,"/",files[i]),
+                             file.path(path,folder,files[i]))
+                }
+                #message(paste0("Downloading:", files[i], " of ", length(files), " files"))
+
             }
         }
     }
--- R/tcga.R
+++ R/tcga.R
@@ -186,6 +186,15 @@ tcgaSearch <- function(tumor = NULL, platform = NULL, added.since = NULL,
     }
     return(db)
 }
+# @description get the arhive info from tcga api
+getArchive <- function(id) {
+    root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
+    query <- paste0("query=Archive&FileInfo[@id=", id,
+                    "]&roleName=archiveCollection")
+    url <- paste0(root, query)
+    db <- tcgaGetTable(url)
+    return(db)
+}
 
 getSamplesFiles <- function(id) {
     archives <- NULL
@@ -225,7 +234,7 @@ getSamplesFiles <- function(id) {
 getBcrArchiveCollection <- function(id) {
     tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
     tcga.query <- paste0("query=Archive&BiospecimenBarcode[@id=",
-                        id, "]&roleName=bcrArchiveCollection")
+                         id, "]&roleName=bcrArchiveCollection")
     url <- paste0(tcga.root, tcga.query)
     db <- tcgaGetTable(url)
     db$addedDate <- as.Date(db$addedDate, "%m-%d-%Y")
@@ -234,16 +243,17 @@ getBcrArchiveCollection <- function(id) {
 getBarcodeTable <- function(barcode) {
     tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
     tcga.query <- paste0("query=BiospecimenBarcode[@barcode=",
-                        barcode, "][@isValid=true]")
+                         barcode, "][@isValid=true]")
     url <- paste0(tcga.root, tcga.query)
     db <- tcgaGetTable(url)
-    return (db)
+    return(db)
 }
 # This function created tcga.db warning to create the db
 # takes almost 3 days to update it should take some minutes
 # (function still needed to be done)
 #' @importFrom downloader download
-createTcgaTable <- function(disease = NULL, platform = NULL, type = NULL) {
+createTcgaTable <- function(disease = NULL, platform = NULL,
+                            type = NULL, center = NULL) {
     # get all compressed archives
     tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
     message("Downloading TCGA database")
@@ -252,23 +262,28 @@ createTcgaTable <- function(disease = NULL, platform = NULL, type = NULL) {
     extra <- ""
     pages <- 32
     if (!is.null(platform)) {
-        extra <- paste0(extra, "[Platform[@name=", platform,
-                        "]]")
+        extra <- paste0(extra, "[Platform[@name=", platform, "]]")
         pages <- pages / 4
     }
     if (!is.null(disease)) {
-        extra <- paste0(extra, "[Disease[@abbreviation=", disease,
-                        "]]")
+        extra <- paste0(extra, "[Disease[@abbreviation=", disease, "]]")
         pages <- pages / 4
     }
     if (!is.null(type)) {
-        extra <- paste0(extra, "[ArchiveType[@type=Level_", type,
-                        "]]")
+        if (is.numeric(type)) {
+            extra <- paste0(extra, "[ArchiveType[@type=", type, "]]")
+        } else {
+            extra <- paste0(extra, "[ArchiveType[@type=mage-tab]]")
+        }
+    }
+    if (!is.null(center)) {
+        extra <- paste0(extra, "[Center[@name=", center, "]]")
         pages <- pages / 4
     }
-    url <- paste0(tcga.root, tcga.query, extra)
-    db <- tcgaGetTable(url, pages)
 
+    url <- paste0(tcga.root, tcga.query, extra)
+    print(url)
+    db <- tcgaGetTable(url, pages)
     # remove useless cols
     db <- db[, 1:9]
 
@@ -277,9 +292,13 @@ createTcgaTable <- function(disease = NULL, platform = NULL, type = NULL) {
     if (length(idx) > 0) {
         db <- db[-idx, ]
     }
-    # transoform date into same format
-    db$addedDate <- as.Date(db$addedDate, "%m-%d-%Y")
-    db <- tcgaDbAddCol(db)
+
+    if (!is.null(db)) {
+        # transoform date into same format
+        db$addedDate <- as.Date(db$addedDate, "%m-%d-%Y")
+        db <- tcgaDbAddCol(db)
+        db <- db[order(db$Disease,db$Platform,db$Center),]
+    }
     return(db)
 }
 
@@ -287,8 +306,10 @@ createTcgaTable <- function(disease = NULL, platform = NULL, type = NULL) {
 tcgaDbAddCol <- function(data) {
     disease.table  <- get("disease.table")
     platform.table  <- get("platform.table")
+    center.table  <- get("center.table")
     data$Platform <- ""
     data$Disease <- ""
+    data$Center <- ""
     diseases <- disease.table$abbreviation
     for (i in seq_along(diseases)) {
         idx <- grep(diseases[i], data$baseName)
@@ -298,11 +319,21 @@ tcgaDbAddCol <- function(data) {
     }
 
     for (i in seq_along(platform.table$name)) {
-        idx <- grep(platform.table[i, ]$name, data$baseName)
-        if (length(idx) > 0) {
+        idx <- grepl(platform.table[i, ]$name, data$baseName)
+        idx <- idx & (data$Platform == "")
+        if (any(idx)) {
             data[idx, ]$Platform <- platform.table[i, ]$name
         }
     }
+
+    for (i in seq_along(center.table$name)) {
+        idx <- grepl(center.table[i, ]$name, data$baseName)
+        idx <- idx & (data$Center == "")
+        if (any(idx)) {
+            data[idx, ]$Center <- center.table[i, ]$name
+        }
+    }
+
     return(data)
 }
 
@@ -311,7 +342,10 @@ tcgaDbAddCol <- function(data) {
 #' @importFrom stringr str_match
 #' @importFrom downloader download
 tcgaGetTable <- function(url, max = 0) {
-    db <- NULL
+    db <- data.frame()
+    #nbcon <- get("con")
+    #firstCon <- get("timeStamp")
+
     next.url <- url
     regex <- "<table summary=\"Data Summary\".*</a></td></tr></table>"
     next.regex <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML.*Next"
@@ -322,6 +356,11 @@ tcgaGetTable <- function(url, max = 0) {
     }
     # print(url)
     while (!is.na(next.url)) {
+        # As we have a limitation of 1000 connections
+        # every 3 minutes, we need to verify it
+        #nbcon <- nbcon + 1
+        #if(-firstCon)
+
         download(next.url, "tcga.html", quiet = TRUE)
         html <- readLines("tcga.html")
         match <- str_match(html, regex)
@@ -335,13 +374,9 @@ tcgaGetTable <- function(url, max = 0) {
         colnames(table) <- table[1, ]
         table <- table[-1, ]
 
-        if (exists("db")) {
-            db <- rbind(db, table)
-        } else {
-            db <- table
-        }
+        db <- rbind(db, table)
         # get next table
-        next.url <- str_match(html, next.regex)[6, ]
+        next.url <- str_match(html, next.regex)[idx, ]
         next.url <- gsub("amp;", "", gsub("\">Next", "", next.url))
 
         if (max > 0) {
@@ -354,5 +389,58 @@ tcgaGetTable <- function(url, max = 0) {
         setTxtProgressBar(pb, max)
         close(pb)
     }
+    #assign(con, nbcon, envir=cacheEnv)
     return(db)
+}
+
+
+tcgaUpdate <- function(){
+
+    tcga.db <-  get("tcga.db")
+
+    # get new version of files
+    new.db <-  createTcgaTable()
+    print(dim(new.db))
+    # copy not modified ones
+    for (i in seq_along(new.db[,1])){
+        db <- subset(tcga.db,new.db[i,"name"] == tcga.db$name)
+        new.db[i,"deployStatus"] <- db$barcode
+    }
+
+    idx <- ((new.db$deployStatus == "" |  new.db$deployStatus == "Not found" |
+                 (new.db$deployStatus == "Available")) &
+                !grepl("aux|mage-tab", new.db$name)
+    )
+    new.db[idx,]$deployStatus <- "Available"
+    new.db[idx,]$deployStatus <- getBarcode(new.db[idx,])$barcode
+    colnames(new.db)[4] <- "barcode"
+    tcga.db <- new.db
+    tcga.root <- "http://tcga-data.nci.nih.gov/tcgadccws/GetHTML?"
+
+    # Get platform table
+    tcga.query <- "query=Platform"
+    next.url <- paste0(tcga.root, tcga.query)
+    platform.table <- tcgaGetTable(next.url)
+    platform.table <- platform.table[, 1:4]
+    platform.table <- platform.table[order(platform.table$name,
+                                           decreasing = TRUE),]
+
+    # Get disease table
+    tcga.query <- "query=Disease"
+    next.url <- paste0(tcga.root, tcga.query)
+    disease.table <- tcgaGetTable(next.url)
+    disease.table <- disease.table[, 1:3]
+
+    # Get center table
+    tcga.query <- "query=Center"
+    next.url <- paste0(tcga.root, tcga.query)
+    center.table  <- tcgaGetTable(next.url)
+    center.table <- center.table[, 1:3]
+
+    save(platform.table, disease.table, tcga.db, center.table,
+         file = paste0(system.file("extdata", package = "TCGAbiolinks"),
+                       "/dataFolders.rda")
+    )
+    file = system.file("extdata/dataFolders.rda",package = "TCGAbiolinks")
+    load(file, envir = as.environment("package:TCGAbiolinks"))
 }
