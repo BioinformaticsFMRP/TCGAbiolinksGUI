f6c650bf6477456b994f0da3c0d8537786ae3790
tiagochst <tiagochst@gmail.com>
2016-04-26 10:11:33 -0300

    removing examples

--- inst/examples/biomicsSearch.R
+++
@@ -1,9 +0,0 @@
-
-query <- biOmics::biOmicsSearch("brain")
-
-# Experiments
-# "Microarray"-"ExpressionArray"-"ExonArray"-"RNASeq"
-# "MiRNAMicroArray"-"Firehose"-"DNAMethylation "-"miRNASeq"-"RRBS"
-# "ChipSeq"-"MRESeq"-"Rampage"-"DNAsequencing"
-# "fiveC"-RepliSeq"-"Others"
-query <- biOmics::biOmicsSearch("brain", experiment = "ExpressionArray")
--- inst/examples/roadmapSearch.R
+++
@@ -1,8 +0,0 @@
-query <- roadmapSearch(sample = "H1 cell line")
-
-query <- roadmapSearch(sample = "H1 cell line", experiment = "RRBS")
-
-query <- roadmapSearch(accession = "GSM621357")
-
-query <- roadmapSearch(sample = c("H1 cell line","H9 cell line"),
-                       experiment = c("RRBS","H3K79me2"))
