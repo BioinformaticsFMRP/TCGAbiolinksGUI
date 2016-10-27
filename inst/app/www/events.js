4560dda106142b454529c67f92540bb88565e325
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-10-27 11:24:29 -0700

    Adding missing file

---
+++ inst/app/www/events.js
@@ -0,0 +1,37 @@
+$(function() {
+  $(document).on({
+
+    'shiny:connected': function(event) {
+      $('form.well').fadeOut(3000).fadeIn(2000);
+    },
+
+    'shiny:disconnected': function(event) {
+      alert('Disconnected! The web socket state is ' + event.socket.readyState);
+    },
+
+    'shiny:busy': function(event) {
+    //    console.log(event)
+      //$('#busyModal').modal('show');
+    },
+
+    'shiny:idle': function(event) {
+      //$('#busyModal').modal('hide');
+    },
+
+    'shiny:recalculating': function(event) {
+      console.log('An output is being recalculated... ' + new Date());
+      $('#busyModal').modal('show');
+    },
+
+    'shiny:recalculated': function(event) {
+      console.log('An output has been recalculated! ' + new Date());
+      $('#busyModal').modal('hide');
+    }
+
+  });
+
+
+  Shiny.addCustomMessageHandler('special', function(message) {
+    //
+  });
+});
