4a4f9c00649df5c4a4648b91259b54b6c100c525
Tiago Chedraoui Silva <tiagochst[at]gmail.com>
2016-10-28 15:34:34 -0700

    Editing SE

--- inst/app/server.R
+++ inst/app/server.R
@@ -284,6 +284,139 @@ TCGAbiolinksGUIServer <- function(input, output, session) {
         return(df)
     })
 
+    #-------------------------------------------------------------------------
+    #                            Summarized experiment edition
+    #-------------------------------------------------------------------------
+
+    seeditdata <- reactive({
+        inFile <- input$seeditfile
+        if (is.null(inFile)) return(NULL)
+        file  <- as.character(parseFilePaths(get.volumes(isolate({input$workingDir})), input$seeditfile)$datapath)
+        if(tools::file_ext(file)=="rda"){
+            se <- get(load(file))
+            return(se)
+        }
+        return(se)
+    })
+
+    output$downloadData <- downloadHandler(
+        filename = function() {
+            file  <- basename(as.character(parseFilePaths(get.volumes(isolate({input$workingDir})), input$seeditfile)$datapath))
+            paste0("samples-matrix-",gsub("rda",'csv',file))
+        },
+        content = function(con) {
+            if(!is.null(seeditdata())) {
+                data <- as.data.frame(colData(seeditdata()))
+                file  <- as.character(parseFilePaths(get.volumes(isolate({input$workingDir})), input$seeditfile)$datapath)
+                row.names <- rownames(data)
+                write_csv(cbind(row.names,data), con)
+            }
+        }
+    )
+    observeEvent(input$seeditSEbt, {
+        data <- seeditdata()
+        closeAlert(session,"seeditAlert")
+        inFile <- isolate({input$seuploadfile})
+        if (is.null(inFile))
+            return(NULL)
+        sample.data <-   read_csv(inFile$datapath,col_names = TRUE)
+        rownames(sample.data) <- sample.data$row.names
+        sample.data$row.names <- NULL
+        colData(data) <- DataFrame(sample.data)
+        getPath <- parseDirPath(get.volumes(isolate({input$workingDir})), input$workingDir)
+        if (length(getPath) == 0) getPath <- paste0(Sys.getenv("HOME"),"/TCGAbiolinksGUI")
+        filename <- file.path(getPath,isolate({input$seeditfilename}))
+        withProgress(message = 'Saving new summarized Experiment object',
+                     detail = 'This may take a while...', value = 0, {
+                         save(data, file = filename)
+                         incProgress(1, detail = "Completed")
+
+                     })
+        createAlert(session, "seeditAlert", "seeditAlertMessage", title = "File created", style =  "success",
+                    content = paste0("Saved as: ",filename), append = FALSE)
+    })
+    observeEvent(input$seeditfile, {
+
+        updateTextInput(session,"seeditfilename", value = paste0("edited_",
+                                                                 basename(as.character(parseFilePaths(get.volumes(isolate({input$workingDir})), input$seeditfile)$datapath))))
+
+        updateCollapse(session, "collapseseeEdit", open = "Sample data matrix")
+
+        output$seeditassaytb <- renderDataTable({
+            x <- as.data.frame(assay(seeditdata()))
+            row.names <- rownames(x)
+            x <- cbind(row.names,x)
+            return(x)
+        },
+        options = list(pageLength = 10,
+                       scrollX = TRUE,
+                       jQueryUI = TRUE,
+                       pagingType = "full",
+                       lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
+                       language.emptyTable = "No results found",
+                       "dom" = 'T<"clear">lfrtip',
+                       "oTableTools" = list(
+                           "sSelectedClass" = "selected",
+                           "sRowSelect" = "os",
+                           "sSwfPath" = paste0("//cdn.datatables.net/tabletools/2.2.4/swf/copy_csv_xls.swf"),
+                           "aButtons" = list(
+                               list("sExtends" = "collection",
+                                    "sButtonText" = "Save",
+                                    "aButtons" = c("csv","xls")
+                               )
+                           )
+                       )
+        ), callback = "function(table) {table.on('click.dt', 'tr', function() {Shiny.onInputChange('allRows',table.rows('.selected').data().toArray());});}"
+        )
+
+        output$seeditfeaturetb <- renderDataTable({
+            return(as.data.frame(rowRanges(seeditdata())))
+        },
+        options = list(pageLength = 10,
+                       scrollX = TRUE,
+                       jQueryUI = TRUE,
+                       pagingType = "full",
+                       lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
+                       language.emptyTable = "No results found",
+                       "dom" = 'T<"clear">lfrtip',
+                       "oTableTools" = list(
+                           "sSelectedClass" = "selected",
+                           "sRowSelect" = "os",
+                           "sSwfPath" = paste0("//cdn.datatables.net/tabletools/2.2.4/swf/copy_csv_xls.swf"),
+                           "aButtons" = list(
+                               list("sExtends" = "collection",
+                                    "sButtonText" = "Save",
+                                    "aButtons" = c("csv","xls")
+                               )
+                           )
+                       )
+        ), callback = "function(table) {table.on('click.dt', 'tr', function() {Shiny.onInputChange('allRows',table.rows('.selected').data().toArray());});}"
+        )
+
+        output$seeditsampletb <- renderDataTable({
+            return(as.data.frame(colData(seeditdata())))
+        },
+        options = list(pageLength = 10,
+                       scrollX = TRUE,
+                       jQueryUI = TRUE,
+                       pagingType = "full",
+                       lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
+                       language.emptyTable = "No results found",
+                       "dom" = 'T<"clear">lfrtip',
+                       "oTableTools" = list(
+                           "sSelectedClass" = "selected",
+                           "sRowSelect" = "os",
+                           "sSwfPath" = paste0("//cdn.datatables.net/tabletools/2.2.4/swf/copy_csv_xls.swf"),
+                           "aButtons" = list(
+                               list("sExtends" = "collection",
+                                    "sButtonText" = "Save",
+                                    "aButtons" = c("csv","xls")
+                               )
+                           )
+                       )
+        ), callback = "function(table) {table.on('click.dt', 'tr', function() {Shiny.onInputChange('allRows',table.rows('.selected').data().toArray());});}"
+        )
+    })
 
     #-------------------------------------------------------------------------
     #                            TCGA Search
@@ -863,9 +996,11 @@ TCGAbiolinksGUIServer <- function(input, output, session) {
                         restrictions=system.file(package='base'), filetypes=c('', 'maf',"csv","maf.gz"))
         shinyFileChoose(input, 'mafAnnotation', roots=get.volumes(input$workingDir), session=session,
                         restrictions=system.file(package='base'), filetypes=c('', 'csv','rda'))
+        shinyFileChoose(input, 'seeditfile', roots=get.volumes(input$workingDir), session=session,
+                        restrictions=system.file(package='base'), filetypes=c('','rda'))
         shinyFileChoose(input, 'oncoGenesFiles', roots=get.volumes(input$workingDir), session=session, restrictions=system.file(package='base'))
     })
-    annotation.maf <- function(){
+    annotation.maf <- reactive({
         inFile <- input$mafAnnotation
         if (is.null(inFile)) return(NULL)
         file  <- as.character(parseFilePaths(get.volumes(isolate({input$workingDir})), input$mafAnnotation)$datapath)
@@ -882,7 +1017,7 @@ TCGAbiolinksGUIServer <- function(input, output, session) {
             return(NULL)
         }
         return(se)
-    }
+    })
     mut <-  reactive({
         #inFile <- input$maffile
         #if (is.null(inFile)) return(NULL)
--- inst/app/ui.R
+++ inst/app/ui.R
@@ -83,6 +83,7 @@ sidebar <-  dashboardSidebar(
                  menuSubItem("Clinical data" , tabName = "tcgaClinical", icon = icon("database")),
                  menuSubItem("Subtype data" , tabName = "tcgaSubtype", icon = icon("database"))
         ),
+        menuItem("Manage Summarized Experiment" , tabName = "seedit", icon = icon("pencil")),
         tags$hr(class="lineAnalysis"),
         menuItem("Clinical analysis", icon = icon("flask"),
                  menuSubItem("Survival plot" , tabName = "tcgasurvival", icon = icon("picture-o"))
@@ -278,6 +279,48 @@ body <-  dashboardBody(
                                             width: 100%",
                                             icon = icon("cogs")))
                     ))
+        ),
+        tabItem(tabName = "seedit",
+                fluidRow(
+                    column(8, bsAlert("seeditAlert"),
+                           bsCollapse(id = "collapseseeEdit", open = "Description of the data",
+                                      bsCollapsePanel("Sample matrix", dataTableOutput('seeditsampletb'), style = "default"),
+                                      bsCollapsePanel("Feature matrix", dataTableOutput('seeditfeaturetb'), style = "default"),
+                                      bsCollapsePanel("Assay matrix", dataTableOutput('seeditassaytb'), style = "default")
+                           )),
+                    column(4,
+                           box(title = "Editing summarized experiment",width = NULL,
+                               status = "danger",
+                               solidHeader = FALSE, collapsible = FALSE,
+                               box(title = "Data",width = NULL,
+                                   solidHeader = TRUE, collapsible = TRUE, collapsed = TRUE,
+                               shinyFilesButton('seeditfile', 'Select Summarized Experiment file', 'Please select summarized experiment object (.rda)', multiple = FALSE)
+                               ),
+                               box(title = "Update sample matrix",width = NULL,
+                                   solidHeader = TRUE, collapsible = TRUE,
+                               downloadButton('downloadData', 'Download sample data matrix'),
+                               tags$br(),
+                               tags$br(),
+                               fileInput('seuploadfile', 'Select a csv to update sample data matrix in the SE',
+                                         accept = c(
+                                             'text/csv',
+                                             'text/comma-separated-values',
+                                             'text/tab-separated-values',
+                                             'text/plain',
+                                             '.csv',
+                                             '.rda'
+                                         )
+                               ),
+                               textInput("seeditfilename", "Save as:", value = "edited_se.rda", width = "100%", placeholder = "Name of the new SummarizedExperiment file (.rda)"),
+                               actionButton("seeditSEbt",
+                                            "Save new Summarized Experiment (.rda) with the uploaded file",
+                                            style = "background-color: #000080;
+                                            color: #FFFFFF;
+                                            margin-left: auto;
+                                            margin-right: auto;
+                                            width: 100%",
+                                            icon = icon("floppy-o")))
+                           )))
         ),
         tabItem(tabName = "tcgaClinical",
                 fluidRow(
