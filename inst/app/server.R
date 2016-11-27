d170deb5da8215cd8adeacc230f5560c58757e03
tiago <tiagochst@gmail.com>
2016-11-26 20:05:29 -0800

    Bug fix if files are not right in oncoprint

--- inst/app/server.R
+++ inst/app/server.R
@@ -1218,7 +1218,19 @@ TCGAbiolinksGUIServer <- function(input, output, session) {
                 closeAlert(session, "oncoAlert")
             }
 
-
+            if(!is.null(annotation)){
+                if(!"bcr_patient_barcode" %in% colnames(annotation)){
+                createAlert(session, "oncomessage", "oncoAlert", title = "Error", style =  "danger",
+                            content = "bcr_patient_barcode column should be in the annotation", append = TRUE)
+                    return(NULL)
+                }
+                idx <- match(substr(mut$Tumor_Sample_Barcode,1,12),annotation$bcr_patient_barcode)
+                if(all(is.na(idx))){
+                    createAlert(session, "oncomessage", "oncoAlert", title = "Error", style =  "danger",
+                                content = "Annotation samples does not match mutation", append = TRUE)
+                    return(NULL)
+                }
+            }
 
             withProgress(message = 'Creating plot',
                          detail = 'This may take a while...', value = 0, {
