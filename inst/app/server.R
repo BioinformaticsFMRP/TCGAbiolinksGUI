d839ae964564caaa77fc75cf95b488c9ba95b4ed
tiago <tiagochst@gmail.com>
2017-02-20 21:37:24 -0300

    Bug fix

--- inst/app/server.R
+++ inst/app/server.R
@@ -262,6 +262,7 @@ TCGAbiolinksGUIServer <- function(input, output, session) {
     source(file.path(server.path, "survival.R"),  local = TRUE)$value
     source(file.path(server.path, "volcano.R"),  local = TRUE)$value
     source(file.path(server.path, "heatmap.R"),  local = TRUE)$value
+    source(file.path(server.path, "dmr.R"),  local = TRUE)$value
     source(file.path(server.path, "dea.R"),  local = TRUE)$value
     source(file.path(server.path, "pathview.R"),  local = TRUE)$value
     source(file.path(server.path, "oncoprint.R"),  local = TRUE)$value
