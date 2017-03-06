26091f6dde3383c139a1ade87fe54558dfb9aec0
tiago <tiagochst@gmail.com>
2017-03-06 07:26:16 -0600

    Fixing table

--- inst/app/server/getinference.R
+++ inst/app/server/getinference.R
@@ -14,69 +14,69 @@ observe({
 # Network inference
 #=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=--=
 observeEvent(input$networkBt , {
-# output$tcgaNetworktbl <- renderDataTable({
-    # Close in case it is open
-    closeAlert(session, "networkAlert")
-    print("Start inference")
+    output$tcgaNetworktbl <- renderDataTable({
+        # Close in case it is open
+        closeAlert(session, "networkAlert")
+        print("Start inference")
 
-    data <- networkdata()
+        data <- networkdata()
 
-    # Input should be expression data with genes in columns and samples in rows
-    data <- t(data)
+        # Input should be expression data with genes in columns and samples in rows
+        data <- t(data)
 
-    method <- isolate({input$networkInferenceMethodRb})
-    withProgress(message = 'Start inference',
-                 detail = 'This may take a while...', value = 0, {
-                     myadj <- TCGAanalyze_networkInference(data, optionMethod=method)
-                 })
+        method <- isolate({input$networkInferenceMethodRb})
+        withProgress(message = 'Start inference',
+                     detail = 'This may take a while...', value = 0, {
+                         myadj <- TCGAanalyze_networkInference(data, optionMethod=method)
+                     })
 
-    # Saving output
-    out.filename <- paste0(paste("network_results_method", method,sep="_"),".rda")
-    getPath <- parseDirPath(get.volumes(isolate({input$workingDir})), input$workingDir)
-    if (length(getPath) == 0) getPath <- paste0(Sys.getenv("HOME"),"/TCGAbiolinksGUI")
+        # Saving output
+        out.filename <- paste0(paste("network_results_method", method,sep="_"),".rda")
+        getPath <- parseDirPath(get.volumes(isolate({input$workingDir})), input$workingDir)
+        if (length(getPath) == 0) getPath <- paste0(Sys.getenv("HOME"),"/TCGAbiolinksGUI")
 
-    save(myadj, file = out.filename)
+        save(myadj, file = out.filename)
 
-    createAlert(session, "networkmessage", "networkAlert", title = "Analysis completed", style =  "success",
-                content = paste0("File saved in: ",out.filename), append = FALSE)
+        createAlert(session, "networkmessage", "networkAlert", title = "Analysis completed", style =  "success",
+                    content = paste0("File saved in: ",out.filename), append = FALSE)
 
-    vals <- sort(myadj[upper.tri(myadj)],decreasing=TRUE)[1:10]
-    
-    tmp <- NULL
-    for(i in 1:10){
-        ind <-  which(myadj == vals[i], arr.ind = TRUE)
-        ind <- ind[seq(1,length(ind),2)]
-        tmp <- rbind(tmp, ind)
-    }
+        vals <- sort(myadj[upper.tri(myadj)],decreasing=TRUE)[1:10]
 
-    tbl <- data.frame("gene 1"=rep(NA, nrow(tmp)),"gene 2"=rep(NA, nrow(tmp)),"value"=rep(NA, nrow(tmp)))
-    for(i in 1:nrow(tmp)){
-        tbl[i,] <- c(rownames(myadj)[tmp[i,1]], rownames(myadj)[tmp[i,2]], myadj[tmp[i,1],tmp[i,2]])
-    }
+        tmp <- NULL
+        for(i in 1:10){
+            ind <-  which(myadj == vals[i], arr.ind = TRUE)
+            ind <- ind[seq(1,length(ind),2)]
+            tmp <- rbind(tmp, ind)
+        }
 
-    return(tbl)
-    # },
-    # options = list(pageLength = 10,
-    #                scrollX = TRUE,
-    #                jQueryUI = TRUE,
-    #                pagingType = "full",
-    #                autoWidth = TRUE,
-    #                lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
-    #                language.emptyTable = "No results found",
-    #                "dom" = 'T<"clear">lfrtip',
-    #                "oTableTools" = list(
-    #                    "sSelectedClass" = "selected",
-    #                    "sRowSelect" = "os",
-    #                    "sSwfPath" = paste0("//cdn.datatables.net/tabletools/2.2.4/swf/copy_csv_xls.swf"),
-    #                    "aButtons" = list(
-    #                        list("sExtends" = "collection",
-    #                             "sButtonText" = "Save",
-    #                             "aButtons" = c("csv","xls")
-    #                        )
-    #                    )
-    #                )
-    # ), callback = "function(table) {table.on('click.dt', 'tr', function() {Shiny.onInputChange('allRows',table.rows('.selected').data().toArray());});}"
-    # )
+        tbl <- data.frame("gene 1"=rep(NA, nrow(tmp)),"gene 2"=rep(NA, nrow(tmp)),"value"=rep(NA, nrow(tmp)))
+        for(i in 1:nrow(tmp)){
+            tbl[i,] <- c(rownames(myadj)[tmp[i,1]], rownames(myadj)[tmp[i,2]], myadj[tmp[i,1],tmp[i,2]])
+        }
+
+        return(tbl)
+    },
+    options = list(pageLength = 10,
+                   scrollX = TRUE,
+                   jQueryUI = TRUE,
+                   pagingType = "full",
+                   autoWidth = TRUE,
+                   lengthMenu = list(c(10, 20, -1), c('10', '20', 'All')),
+                   language.emptyTable = "No results found",
+                   "dom" = 'T<"clear">lfrtip',
+                   "oTableTools" = list(
+                       "sSelectedClass" = "selected",
+                       "sRowSelect" = "os",
+                       "sSwfPath" = paste0("//cdn.datatables.net/tabletools/2.2.4/swf/copy_csv_xls.swf"),
+                       "aButtons" = list(
+                           list("sExtends" = "collection",
+                                "sButtonText" = "Save",
+                                "aButtons" = c("csv","xls")
+                           )
+                       )
+                   )
+    ), callback = "function(table) {table.on('click.dt', 'tr', function() {Shiny.onInputChange('allRows',table.rows('.selected').data().toArray());});}"
+    )
 })
 
 
