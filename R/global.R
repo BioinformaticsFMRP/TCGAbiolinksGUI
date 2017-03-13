b312e2a240f635db7b27f0267c10a2fbf7f22082
Tiago Chedraoui Silva <tiagochst@usp.br>
2017-03-13 01:18:30 +0000

    update
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/TCGAbiolinksGUI@127334 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/global.R
+++ R/global.R
@@ -9,10 +9,11 @@
 #' @name TCGAbiolinksGUI
 #' @import shiny shinyFiles shinydashboard downloader
 #' TCGAbiolinks ggplot2 shinyBS stringr ggrepel pathview ELMER grid
-#' clusterProfiler parallel readr data.table googleVis
+#' clusterProfiler parallel readr data.table googleVis plotly
 #' @importFrom SummarizedExperiment SummarizedExperiment values rowRanges colData<- assay colData
-#' @importFrom  colourpicker colourInput
+#' @importFrom colourpicker colourInput
 #' @importFrom shinyjs hide show toggle useShinyjs
+#' @importFrom plotly plotlyOutput renderPlotly renderPlotly layout add_pie
 #' @export
 #' @return Open a connection to shiny
 TCGAbiolinksGUI <- function(run = TRUE) {
