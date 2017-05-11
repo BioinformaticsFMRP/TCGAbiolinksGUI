3386d174f89265dd7580c652771c57dea906e4aa
tiagochst <tiagochst@gmail.com>
2017-05-11 13:46:33 -0300

    Adding number of samples plot closes #14

--- inst/app/server/getmolecular.R
+++ inst/app/server/getmolecular.R
@@ -192,38 +192,64 @@ observeEvent(input$tcgaSearchBt, {
         # be downloaded
         if(is.null(results)) return(NULL)
         legacy <- isolate({as.logical(input$tcgaDatabase)})
+        nb.samples <- gvisBarChart(data.frame("Samples" = nrow(results),
+                                              "Project" = unique(results$project),
+                                              "Samples.annotation" = nrow(results)),
+                                   xvar = "Project",
+                                   yvar = c("Samples","Samples.annotation"),
+                                   options=list( title="Number of samples", legend="{position:'none'}",
+                                                 hAxis = "{
+                                                     minValue: 0, textPosition: 'none',
+                                                     gridlines: {
+                                                         color:'transparent'
+                                                     }
+                                                 }"))
+        file.size <- gvisBarChart(data.frame("Size" = sum(results$file_size/(2^30)) ,
+                                             "Project" = unique(results$project),
+                                             "Size.annotation" = paste0(round(sum(results$file_size/(2^30)), digits = 2), " GB")),
+                                  xvar = "Project",
+                                  yvar = c("Size","Size.annotation"),
+                                  options=list( title="Total size of files", legend="{position:'none'}",
+                                                hAxis = "{
+                                                     minValue: 0, textPosition: 'none',
+                                                     gridlines: {
+                                                         color:'transparent'
+                                                     }
+                                                 }"))
 
         data.type <- gvisPieChart(as.data.frame(table(results$data_type)),
-                                  options=list( title="Data type", pieSliceText = "value"))
+                                  options=list( title="Data type", pieSliceText = "percentage"))
         tissue.definition <- gvisPieChart(as.data.frame(table(results$tissue.definition)),
-                                          options=list( title="Tissue definition",pieSliceText = "value"))
+                                          options=list( title="Tissue definition",pieSliceText = "percentage"))
         experimental_strategy  <- gvisPieChart(as.data.frame(table(results$experimental_strategy )),
-                                               options=list( title="Experimental strategy", pieSliceText = "value"))
+                                               options=list( title="Experimental strategy", pieSliceText = "percentage"))
         analysis.workflow_type <- gvisPieChart(as.data.frame(table(paste0(
             results$analysis_workflow_type,"(", results$analysis_workflow_link, ")"))),
-            options=list( title="Workflow type", pieSliceText = "value"))
+            options=list( title="Workflow type", pieSliceText = "percentage"))
         data.category <- gvisPieChart(as.data.frame(table(results$data_category)),
-                                      options=list( title="Data category", pieSliceText = "value"))
+                                      options=list( title="Data category", pieSliceText = "percentage"))
 
         gender.plot <- gvisPieChart(as.data.frame(table(clinical$gender)),
-                                    options=list( title = "Gender", pieSliceText = "value"))
+                                    options=list( title = "Gender", pieSliceText = "percentage"))
         race.plot <- gvisPieChart(as.data.frame(table(clinical$race)),
-                                  options=list( title = "Race", pieSliceText = "value"))
+                                  options=list( title = "Race", pieSliceText = "percentage"))
         vital.status.plot <- gvisPieChart(as.data.frame(table(clinical$vital_status)),
-                                          options=list( title="Vital status", pieSliceText = "value"))
+                                          options=list( title="Vital status", pieSliceText = "percentage"))
         tumor.stage.plot <- gvisPieChart(as.data.frame(table(clinical$tumor_stage)),
-                                         options=list( title="Tumor stage", pieSliceText = "value"))
+                                         options=list( title="Tumor stage", pieSliceText = "percentage"))
 
         clinical.plots <- gvisMerge(gvisMerge(tumor.stage.plot,vital.status.plot , horizontal = TRUE),
                                     gvisMerge(race.plot, gender.plot, horizontal = TRUE))
 
         if(!legacy) {
-            data.plots <- gvisMerge(gvisMerge(tissue.definition, data.type, horizontal = TRUE),
-                                    gvisMerge(experimental_strategy, analysis.workflow_type, horizontal = TRUE))
-
+            data.plots <- gvisMerge(
+                gvisMerge(gvisMerge(nb.samples, file.size, horizontal = TRUE),
+                          gvisMerge(tissue.definition, experimental_strategy, horizontal = TRUE)),
+                gvisMerge(data.type, analysis.workflow_type, horizontal = TRUE)
+                )
         } else {
-            data.plots <- gvisMerge(gvisMerge(tissue.definition, data.type, horizontal = TRUE),
-                                    experimental_strategy)
+            data.plots <- gvisMerge(gvisMerge(nb.samples,file.size,, horizontal = TRUE),
+                                    gvisMerge(tissue.definition, data.type, horizontal = TRUE))
         }
         gvisMerge(data.plots,clinical.plots)
     })
